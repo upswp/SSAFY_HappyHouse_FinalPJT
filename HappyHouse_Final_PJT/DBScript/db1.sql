a466889e41526ad683ec1c3f404bdfc02f43e9bf
OHHYEONJEONG <guswjd3927@naver.com>
2020-11-18 16:37:21 +0900

    DB sql 추가

---
+++ HappyHouse_Final_PJT/DBScript/db1.sql
@@ -0,0 +1,146 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: happyhousefinal
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comment` (
+  `commentno` int NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(45) NOT NULL,
+  `content` varchar(500) NOT NULL,
+  `created_date` date NOT NULL,
+  PRIMARY KEY (`commentno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='댓글 DB';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `member`
+--
+
+DROP TABLE IF EXISTS `member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `member` (
+  `userid` int NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `userpwd` varchar(45) NOT NULL,
+  `address` varchar(45) NOT NULL,
+  `birth_date` date DEFAULT NULL,
+  `interest_area` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `marriage_type` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `member`
+--
+
+LOCK TABLES `member` WRITE;
+/*!40000 ALTER TABLE `member` DISABLE KEYS */;
+/*!40000 ALTER TABLE `member` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notice_board`
+--
+
+DROP TABLE IF EXISTS `notice_board`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `notice_board` (
+  `no` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `content` varchar(500) NOT NULL,
+  `writer` varchar(45) NOT NULL,
+  `views` int DEFAULT '0',
+  `regtime` date NOT NULL,
+  PRIMARY KEY (`no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `notice_board`
+--
+
+LOCK TABLES `notice_board` WRITE;
+/*!40000 ALTER TABLE `notice_board` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notice_board` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `qna_board`
+--
+
+DROP TABLE IF EXISTS `qna_board`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `qna_board` (
+  `no` int NOT NULL AUTO_INCREMENT,
+  `writer` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `title` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `content` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `comment_count` int DEFAULT '0',
+  `regtime` date NOT NULL,
+  `comment_no` int DEFAULT NULL,
+  PRIMARY KEY (`no`),
+  KEY `commentno_idx` (`comment_no`),
+  CONSTRAINT `commentno` FOREIGN KEY (`comment_no`) REFERENCES `comment` (`commentno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `qna_board`
+--
+
+LOCK TABLES `qna_board` WRITE;
+/*!40000 ALTER TABLE `qna_board` DISABLE KEYS */;
+/*!40000 ALTER TABLE `qna_board` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'happyhousefinal'
+--
+
+--
+-- Dumping routines for database 'happyhousefinal'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-18 16:33:28
