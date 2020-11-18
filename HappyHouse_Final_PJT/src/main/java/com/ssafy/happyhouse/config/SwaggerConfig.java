d6d91047fcc367d763c3f064e8e954449c24641c
upswp <tkddnsos@gmail.com>
2020-11-18 16:15:44 +0900

    board.xml 수정

--- HappyHouse_Final_PJT/src/main/java/com/ssafy/happyhouse/config/SwaggerConfig.java
+++ HappyHouse_Final_PJT/src/main/java/com/ssafy/happyhouse/config/SwaggerConfig.java
@@ -10,7 +10,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//http://localhost:9999/vue/swagger-ui.html
+//http://localhost:9999/happyhouse/swagger-ui.html
 	
 @Configuration
 @EnableSwagger2
