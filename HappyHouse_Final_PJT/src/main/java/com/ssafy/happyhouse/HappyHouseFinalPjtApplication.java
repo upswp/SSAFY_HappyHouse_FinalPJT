cb71777efb7cfa3f90dbc3dfcfffdcdef2a95bc1
upswp <tkddnsos@gmail.com>
2020-11-18 15:02:39 +0900

    start project

---
+++ HappyHouse_Final_PJT/src/main/java/com/ssafy/happyhouse/HappyHouseFinalPjtApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.happyhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyHouseFinalPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyHouseFinalPjtApplication.class, args);
+	}
+
+}
