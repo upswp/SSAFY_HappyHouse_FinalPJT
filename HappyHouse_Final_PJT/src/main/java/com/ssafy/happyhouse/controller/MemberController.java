5aaeada6f30a2c96d11510a85e8f86c414b36986
upswp <tkddnsos@gmail.com>
2020-11-24 23:22:25 +0900

    member controller update - 회원 CRUD

--- HappyHouse_Final_PJT/src/main/java/com/ssafy/happyhouse/controller/MemberController.java
+++ HappyHouse_Final_PJT/src/main/java/com/ssafy/happyhouse/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.ssafy.happyhouse.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,15 +14,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.happyhouse.dto.MemberDto;
+import com.ssafy.happyhouse.dto.NoticeBoardDto;
 import com.ssafy.happyhouse.service.JwtService;
 import com.ssafy.happyhouse.service.MemberService;
+
+import io.swagger.annotations.ApiOperation;
 
 
 @CrossOrigin(origins = { "*" }, maxAge = 6000)
@@ -36,7 +43,17 @@ public class MemberController {
 	private MemberService memberService;
 	
 	public static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	private static final String SUCCESS = "success";
+	private static final String FAIL = "fail";
 
+	
+	/**
+	 * ====================================================================================================
+	 * =========================================로그인 관련 controller=========================================
+	 * ====================================================================================================
+	 */
+	
+	
 	/**
 	 * 로그인 token 검사
 	 * @param memberDto
@@ -77,7 +94,7 @@ public class MemberController {
 		}
 		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
-
+	
 	/**
 	 * 사용자 회원정보 반환
 	 * @param req
@@ -105,5 +122,84 @@ public class MemberController {
 		}
 		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
+	
+	/**
+	 * ====================================================================================================
+	 * =========================================회원 정보 관리 controller========================================
+	 * ====================================================================================================
+	 */
+	
+	/**
+	 * 모든 회원 정보 리스트를 반환한다.
+	 * @return
+	 * @throws Exception
+	 */
+    @ApiOperation(value = "모든 회원의 정보를 반환한다.", response = List.class)
+	@GetMapping("/selectMember")
+	public ResponseEntity<List<MemberDto>> retrieveBoard() throws Exception {
+		logger.debug("retrieveMember - 호출");
+		return new ResponseEntity<List<MemberDto>>(memberService.retrieveMember(), HttpStatus.OK);
+	}
+
+    /**
+     * 회원 아이디에 대한 상세보기 기능 제공
+     * @param no 선택한 글 번호
+     * @return
+     */
+    @ApiOperation(value = "회원번호에 해당하는 회원의 정보를 반환한다.", response = NoticeBoardDto.class)    
+	@GetMapping("/detailMember/{no}")
+	public ResponseEntity<MemberDto> detailMember(@PathVariable int no) {
+		logger.debug("detailMember - 호출");
+		return new ResponseEntity<MemberDto>(memberService.detailMember(no), HttpStatus.OK);
+	}
+
+    /**
+     * 회원 가입 create
+     * @param board 입력한 게시글 정보 데이터 
+     * @return
+     */
+    @ApiOperation(value = "새로운 회원 정보를 입력한다. 그리고 DB입력 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+	@PostMapping("/joinMember")
+	public ResponseEntity<String> writeMember(@RequestBody MemberDto memberDto) {
+		logger.debug("joinMember - 호출"+memberDto);
+		if (memberService.joinMember(memberDto)) {
+			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		}
+		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+	}
+
+    /**
+     * 회원 아이디 값에 대한 글 정보 수정 
+     * @param board 선택한 게시글에 대한 글 정보
+     * @return
+     */
+    @ApiOperation(value = "회원번호에 해당하는 회원의 정보를 수정한다. 그리고 DB수정 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+	@PutMapping("/updateMember/{no}")
+	public ResponseEntity<String> updateMember(@RequestBody MemberDto memberDto) {
+		logger.debug("updateMember - 호출");
+		logger.debug("" + memberDto);
+		
+		if (memberService.updateMember(memberDto)) {
+			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		}
+		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+	}
+
+    /**
+     * 회원 아이디 대한 정보 삭제
+     * @param no 해당 글 번호
+     * @return
+     */
+    @ApiOperation(value = "회원번호에 해당하는 회원의 정보를 삭제한다. 그리고 DB삭제 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+	@DeleteMapping("/deleteMember/{no}")
+	public ResponseEntity<String> deleteMember(@PathVariable int no) {
+		logger.debug("deleteBoard - 호출");
+		if (memberService.deleteMember(no)) {
+			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		}
+		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+	}
+	
+	
 
 }
