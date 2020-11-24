b6942410a0289db4c9051e46668bc87078a4df0a
upswp <tkddnsos@gmail.com>
2020-11-24 23:22:56 +0900

    member seviceImple update - 회원 CRUD

--- HappyHouse_Final_PJT/src/main/java/com/ssafy/happyhouse/service/MemberServiceImpl.java
+++ HappyHouse_Final_PJT/src/main/java/com/ssafy/happyhouse/service/MemberServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ssafy.happyhouse.service;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ public class MemberServiceImpl implements MemberService {
 
 	@Autowired
 	private SqlSession sqlSession;
+	private MemberMapper memberMapper;
 	
 	@Override
 	public MemberDto login(MemberDto memberDto) throws Exception {
@@ -22,6 +25,31 @@ public class MemberServiceImpl implements MemberService {
 	@Override
 	public String getServerInfo() {
 		return "사용자에게 전달하고 싶은 중요정보";
+	}
+
+	@Override
+	public List<MemberDto> retrieveMember() {
+		return memberMapper.retrieveMember();
+	}
+
+	@Override
+	public MemberDto detailMember(int no) {
+		return memberMapper.detailMember(no);
+	}
+
+	@Override
+	public boolean joinMember(MemberDto memberDto) {
+		return memberMapper.joinMember(memberDto) == 1;
+	}
+
+	@Override
+	public boolean updateMember(MemberDto memberDto) {
+		return memberMapper.updateMember(memberDto) == 1;
+	}
+
+	@Override
+	public boolean deleteMember(int no) {
+		return memberMapper.deleteMember(no) == 1;
 	}
 
 }
