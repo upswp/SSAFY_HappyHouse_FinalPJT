08f6768f545195cfdbd32b1faa89436837a6701c
upswp <tkddnsos@gmail.com>
2020-11-24 23:22:46 +0900

    member service update - 회원 CRUD

--- HappyHouse_Final_PJT/src/main/java/com/ssafy/happyhouse/service/MemberService.java
+++ HappyHouse_Final_PJT/src/main/java/com/ssafy/happyhouse/service/MemberService.java
@@ -1,10 +1,17 @@
 package com.ssafy.happyhouse.service;
 
+import java.util.List;
+
 import com.ssafy.happyhouse.dto.MemberDto;
 
 public interface MemberService {
 
 	public MemberDto login(MemberDto memberDto) throws Exception;
 	public String getServerInfo();
+	public List<MemberDto> retrieveMember();
+	public MemberDto detailMember(int no);
+	public boolean joinMember(MemberDto memberDto);
+	public boolean updateMember(MemberDto memberDto);
+	public boolean deleteMember(int no);
 	
 }
